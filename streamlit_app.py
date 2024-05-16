143a0cb96a867942fe6eeb341ee29579a932a043
Lise LAROCHETTE <168559876+LiseLarochette@users.noreply.github.com>
2024-05-16 19:58:40 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -6,16 +6,6 @@ st.title('Wild Love')
 
 st.write("Découvre ton match idéal")
 
-link = "https://raw.githubusercontent.com/murpi/wilddata/master/quests/cars.csv"
-df_weather = pd.read_csv(link)
-
-# Create a button
-if st.button('Show Picture'):
-    # Display an image when the button is clicked
-    st.image('image.jpg', caption='A beautiful picture')
-
-# You can replace 'image.jpg' with the path to your own image file
-
 import turtle
 
 # Initialiser la fenêtre
@@ -50,3 +40,13 @@ pen.hideturtle()
 
 # Garder la fenêtre ouverte
 window.mainloop()
+
+
+# Create a button
+if st.button('Show Picture'):
+    # Display an image when the button is clicked
+    st.image('image.jpg', caption='A beautiful picture')
+
+# You can replace 'image.jpg' with the path to your own image file
+
+
