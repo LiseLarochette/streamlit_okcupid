4a9bcd8a829fd5471089e19d91595b2e66287a93
Lise LAROCHETTE <168559876+LiseLarochette@users.noreply.github.com>
2024-05-16 19:56:04 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -10,6 +10,8 @@ link = "https://raw.githubusercontent.com/murpi/wilddata/master/quests/cars.csv"
 df_weather = pd.read_csv(link)
 
 # Create a button
-if st.button('Click me'):
-    st.write('Button clicked!')
+if st.button('Show Picture'):
+    # Display an image when the button is clicked
+    st.image('image.jpg', caption='A beautiful picture')
 
+# You can replace 'image.jpg' with the path to your own image file
