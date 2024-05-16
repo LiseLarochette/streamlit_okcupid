0d27ffcc897a79697c55649ef7ab71aaab1967ca
Lise LAROCHETTE <168559876+LiseLarochette@users.noreply.github.com>
2024-05-16 21:28:56 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -17,7 +17,7 @@ users = [
 
 # Fonction pour afficher le titre centré
 def centered_title(text):
-    st.markdown(f"<h1 style='text-align: center; color: #ff4b4b;'>{text}</h1>", unsafe_allow_html=True)
+    st.markdown(f"<h1 style='text-align: center; color: #ff4b4b;'>Wild love</h1>", unsafe_allow_html=True)
 
 def centered_title(text):
     st.markdown(f"<h2 style='text-align: center; color: #ff4b4b;'>For one love</h2>", unsafe_allow_html=True)
